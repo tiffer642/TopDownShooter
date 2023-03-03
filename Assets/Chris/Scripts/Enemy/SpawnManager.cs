cc0e246b0ea8459a90413a0b3852966b10c1947b
tiffer <tiffer642@gmail.com>
2023-03-03 12:10:55 -0800

    EnemySpawner
    Enemy Spawner Works

--- Assets/Chris/Scripts/Enemy/SpawnManager.cs
+++ Assets/Chris/Scripts/Enemy/SpawnManager.cs
@@ -8,19 +8,28 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] spawnPoints;
     GameObject selectedSpawnPoint;
     GameObject selectedEnemy;
+    public GameObject spawnedEnemy;
     int enemyIndex;
     int spawnIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        enemyIndex = Random.Range(0, enemies.Length);
+        spawnIndex = Random.Range(0, spawnPoints.Length);
+        selectedEnemy = enemies[enemyIndex];
+        selectedSpawnPoint = spawnPoints[spawnIndex];
+        print(selectedEnemy.name);
+        print(selectedSpawnPoint.name);
+        Debug.Log(enemyIndex + ", " + spawnIndex);
+        spawnedEnemy = Instantiate(selectedEnemy, selectedSpawnPoint.transform);
+        Debug.Log(spawnedEnemy.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemyIndex = Random.Range(0, enemies.Length);
-        selectedEnemy = enemies[enemyIndex];
+        
     }
 }
